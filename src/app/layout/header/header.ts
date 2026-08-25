b3c5d50301df5c295841583300efb515d94f34ff
kauavidal <kauavidal2004@gmail.com>
2026-08-25 14:33:30 -0300

    feat: badge de quantidade no icone do carrinho do Header

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Logo } from '../../shared/ui/logo/logo';
 import { AuthService } from '../../core/auth/auth.service';
+import { CartService } from '../../core/cart/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -11,5 +12,8 @@ import { AuthService } from '../../core/auth/auth.service';
 })
 export class Header {
   private readonly auth = inject(AuthService);
+  private readonly carrinho = inject(CartService);
+
   protected readonly estaAutenticado = this.auth.estaAutenticado;
+  protected readonly quantidadeCarrinho = this.carrinho.quantidadeTotal;
 }
