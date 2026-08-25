33bdd73ef1e2a9fb2b17bcc5dba52bf348089d3f
kauavidal <kauavidal2004@gmail.com>
2026-08-24 21:58:40 -0300

    feat: componente SectionDivider (motivo entrelacado da marca)

---
+++ src/app/shared/ui/section-divider/section-divider.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-divider',
+  templateUrl: './section-divider.html',
+  styleUrl: './section-divider.scss',
+})
+export class SectionDivider {}
